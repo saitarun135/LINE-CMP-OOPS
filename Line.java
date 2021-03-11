b3c24c38e717dda5238485d0e2f1668d66b2ea57
saitarun135 <saitarun135@gm.com>
2021-03-11 14:59:53 +0530

    equals method implementing

--- Line.java
+++ Line.java
@@ -1,22 +1,39 @@
 import java.util.Scanner;
 	public class Line{
-		 		void length(){
-				int x1,y1,x2,y2;
-            Scanner sc =new Scanner(System.in);
-            System.out.println("Enter x1 value :");
-                x1=sc.nextInt();
-            System.out.println("Enter x2 value :");
-               x2=sc.nextInt();
-            System.out.println("Enter y1 value :");
-               y1=sc.nextInt();
-            System.out.println("Enter y2 value :");
-               y2=sc.nextInt();
+		public static void main(String args[]){
+			int x1,y1,x2,y2;
+	   	int a1,b1,a2,b2;
+			Scanner sc =new Scanner(System.in);
+	   	System.out.println("Enter x1 value :");
+	  			x1=sc.nextInt();
+	   	System.out.println("Enter x2 value :");
+	   		x2=sc.nextInt();
+	   	System.out.println("Enter y1 value :");
+	   		y1=sc.nextInt();
+	   	System.out.println("Enter y2 value :");
+	   		y2=sc.nextInt();
+	      System.out.println("Enter a1 value :");
+	         a1=sc.nextInt();
+	      System.out.println("Enter a2 value :");
+	         a2=sc.nextInt();
+	      System.out.println("Enter b1 value :");
+	         b1=sc.nextInt();
+	      System.out.println("Enter b2 value :");
+	        	b2=sc.nextInt();
 
-   Double line=Math.sqrt((Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)));
-   System.out.println("The length of the line is: "+line);
-			}
-		public static void main(String args[]) {
-			Line ref=new Line();
-			ref.length();
+		Double line1=Math.sqrt((x2-x1)^2+(y2-y1)^2);
+		Double line2=Math.sqrt((a2-a1)^2+(b2-b1)^2);
+		System.out.println("The length of a line-1 is :" +line1);
+		System.out.println("The length of a line-2 is :" +line2);
+				Double d1=(line1);
+				Double d2=(line2);
+				boolean equals=d1.equals(d2);
+				if( equals == true) {
+					System.out.println("Both lines are same");
+						}
+				else {
+					System.out.println("Both lines are not same");
+					}
+		  }
 	}
-}
+	
