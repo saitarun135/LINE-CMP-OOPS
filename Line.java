9692650c8507959e8bf6bb4ba6777f0a24044779
saitarun135 <saitarun135@gm.com>
2021-03-11 15:12:57 +0530

    compareTo method

--- Line.java
+++ Line.java
@@ -21,19 +21,21 @@ import java.util.Scanner;
 	      System.out.println("Enter b2 value :");
 	        	b2=sc.nextInt();
 
-		Double line1=Math.sqrt((x2-x1)^2+(y2-y1)^2);
-		Double line2=Math.sqrt((a2-a1)^2+(b2-b1)^2);
+		Double line1=Math.sqrt((Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)));
+		Double line2=Math.sqrt((Math.pow((a2-a1),2))  + (Math.pow((b2-b1),2)));
 		System.out.println("The length of a line-1 is :" +line1);
 		System.out.println("The length of a line-2 is :" +line2);
 				Double d1=(line1);
 				Double d2=(line2);
-				boolean equals=d1.equals(d2);
-				if( equals == true) {
-					System.out.println("Both lines are same");
-						}
-				else {
-					System.out.println("Both lines are not same");
-					}
-		  }
-	}
-	
+				Double comp=(double) d1.compareTo(d2);
+				if(comp ==1 ) {
+					System.out.println("line1 is higher than line2 ");
+				}
+				if(comp==0){
+					System.out.println("line1 and line2 values are same");
+				}
+				if(comp==-1) {
+					System.out.println("line2 is greater than line1");
+				}
+  }
+}
